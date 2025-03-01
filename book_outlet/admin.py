b7842e16c1a79649a0f0617c3273e59c85c3ba66
ChusongK <quickfailer@gmail.com>
2025-03-01 16:58:38 +0900

    .

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -9,4 +9,5 @@ class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("author", "rating",)
     list_display = ("title", "author")
+    
 admin.site.register(Book, BookAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,12 +5,16 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(models.Model): 
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1),
                                              MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True) 
     is_bestselling = models.BooleanField(default=False)
     # Harry Potter 1 => harry-potter-1
     slug = models.SlugField(default="", blank=True, null=False,
@@ -20,5 +24,5 @@ class Book(models.Model):
         return reverse("book-detail", args=[self.slug])
 
 
-    def __str__(self):
+    def __str__(self): 
         return f"{self.title} ({self.rating})"
