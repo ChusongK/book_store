e7d1d92f126016656191003fee5379a5d073e30e
Chansong <quickfailer@gmail.com>
2025-03-07 20:45:32 +0900

    .

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book, Author
+from .models import Book, Author, Address, Country
 
 # Register your models here.
 
@@ -9,6 +9,9 @@ class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("author", "rating",)
     list_display = ("title", "author")
-    
+
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
